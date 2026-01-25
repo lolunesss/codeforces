92525b7687474e100422efdedf34283ee7aa22b8
Amal <amal@circe.co>
2026-01-26 00:38:22 +0530

    solve 1845/a

---
+++ 1845/a.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k, x;
+    cin >> n >> k >> x;
+    if (x != 1) {
+      cout << "YES" << endl;
+      cout << n << endl;
+      for (int i = 0; i < n; i++) {
+        if (i != 0) cout << " ";
+        cout << 1;
+      }
+      cout << endl;
+    } else {
+      if (k > 2) {
+        if (n % 2 == 0) {
+          cout << "YES" << endl;
+          cout << n / 2 << endl;
+          for (int i = 0; i < n; i += 2) {
+            if (i != 0) cout << " ";
+            cout << 2;
+          }
+          cout << endl;
+        } else {
+          cout << "YES" << endl;
+          int m = (n - 3) / 2 + 1;
+          cout << m << endl;
+          cout << 3;
+          for (int i = 0; i < m - 1; i++) {
+            cout << " " << 2;
+          }
+          cout << endl;
+        }
+      } else {
+        if (k > 1 && n % 2 == 0) {
+          cout << "YES" << endl;
+          cout << n / 2 << endl;
+          for (int i = 0; i < n / 2; i += 1) {
+            if (i != 0) cout << " ";
+            cout << 2;
+          }
+          cout << endl;
+        } else {
+          cout << "NO" << endl;
+        }
+      }
+    }
+  }
+  return 0;
+}
