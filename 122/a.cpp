e26fc6d8d4b8dd96b2478d2522eb1dce044d4738
Amal <amal@circe.co>
2026-03-02 09:42:26 +0530

    solve 122/a

---
+++ 122/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int luckies[] = {4,   7,   44,  47,  74,  77,  444,
+                   447, 474, 477, 744, 747, 774, 777};
+  bool lucky = false;
+  for (int i = 0; i < 14; i++) {
+    lucky = lucky || (n % luckies[i] == 0);
+  }
+  cout << (lucky ? "YES" : "NO") << endl;
+  return 0;
+}
