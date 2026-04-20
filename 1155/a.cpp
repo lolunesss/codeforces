3b272be00a66d4ae192578cc18750a90d2db78b6
Amal <amal@circe.co>
2026-04-21 00:04:43 +0530

    solve 1155/a

---
+++ 1155/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  for (int i = 1; i < n; i++) {
+    if (s[i] < s[i - 1]) {
+      cout << "YES" << endl;
+      cout << i << " " << (i + 1) << endl;
+      return 0;
+    }
+  }
+  cout << "NO" << endl;
+  return 0;
+}
