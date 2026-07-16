bfad7ae1f03a28176852eb2b4d0ee1607dd0a8c9
Amal <amal@circe.co>
2026-07-16 23:50:40 +0530

    solve 2155/a

---
+++ 2155/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int c;
+  cin >> c;
+  while (c--) {
+    int n;
+    cin >> n;
+    cout << (n - 1) + (n - 2) + 1 << endl;
+  }
+  return 0;
+}
