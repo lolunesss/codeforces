5fbbdecd86645bbd6dd1946f3b4fe4c1da3301b9
Amal <amal@circe.co>
2026-07-26 12:38:25 +0530

    solve 1987/a

---
+++ 1987/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int c;
+  cin >> c;
+  while (c--) {
+    int n, k;
+    cin >> n >> k;
+    cout << (n - 1) * k + 1 << endl;
+  }
+  return 0;
+}
